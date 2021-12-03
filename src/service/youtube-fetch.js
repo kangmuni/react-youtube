fb677eee7a575153d42618020e6404504225680d
kangmuni <kangmoonee@gmail.com>
2021-12-03 13:40:32 +0900

    Add axios library and Change service file

---
+++ src/service/youtube-fetch.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+class YoutubeFetch {
+  constructor(key) {
+    this.key = key;
+    this.requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+  }
+
+  //   mostPopular() {
+  //     return fetch(
+  //       `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+  //       this.requestOptions
+  //     )
+  //       .then((response) => response.json())
+  //       .then((result) => result.items);
+  //   }
+
+  async mostPopular() {
+    const response = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+      this.requestOptions
+    );
+    const result = await response.json();
+    return result.items;
+  }
+
+  //   search(query) {
+  //     return fetch(
+  //       `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
+  //       this.requestOptions
+  //     )
+  //       .then((response) => response.json())
+  //       .then((result) =>
+  //         result.items.map((item) => ({ ...item, id: item.id.videoId }))
+  //       );
+  //   }
+
+  async search(query) {
+    const response = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
+      this.requestOptions
+    );
+    const result = await response.json();
+    return result.items.map((item) => ({ ...item, id: item.id.videoId }));
+  }
+}
+
+export default YoutubeFetch;
--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,48 +1,37 @@
 class Youtube {
-  constructor(key) {
-    this.key = key;
-    this.requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-  }
-
-  //   mostPopular() {
-  //     return fetch(
-  //       `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-  //       this.requestOptions
-  //     )
-  //       .then((response) => response.json())
-  //       .then((result) => result.items);
-  //   }
+  // constructor(key) {
+  //   this.youtube = axios.create({
+  //     baseURL: 'https://youtube.googleapis.com/youtube/v3',
+  //     params: { key: key },
+  //   });
+  // }
+  constructor(httpClient) {
+    this.youtube = httpClient;
+  } // dependency injection
 
   async mostPopular() {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-      this.requestOptions
-    );
-    const result = await response.json();
-    return result.items;
+    const response = await this.youtube.get('videos', {
+      params: {
+        part: 'snippet',
+        chart: 'mostPopular',
+        maxResults: 25,
+      },
+    });
+    return response.data.items;
   }
-
-  //   search(query) {
-  //     return fetch(
-  //       `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
-  //       this.requestOptions
-  //     )
-  //       .then((response) => response.json())
-  //       .then((result) =>
-  //         result.items.map((item) => ({ ...item, id: item.id.videoId }))
-  //       );
-  //   }
-
   async search(query) {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
-      this.requestOptions
-    );
-    const result = await response.json();
-    return result.items.map((item) => ({ ...item, id: item.id.videoId }));
+    const response = await this.youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 25,
+        type: 'videos',
+        q: query,
+      },
+    });
+    return response.data.items.map((item) => ({
+      ...item,
+      id: item.id.videoId,
+    }));
   }
 }
 
